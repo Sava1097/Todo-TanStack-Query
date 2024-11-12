f1819a72242286b7d1e42c0d4bb1aa850e4f30e5
Sava1097 <sava10097@gmail.com>
2024-11-12 10:43:07 +0100

    initial todo tanStack project

---
+++ todo-client/src/hooks/useTodos.ts
@@ -0,0 +1,91 @@
+import { useQuery, useMutation, useQueryClient  } from "@tanstack/react-query";
+import { getAllTasks, addTask, toggleTask, deleteTask } from "../api/axiosTasks";
+import type { Task } from "../api/axiosTasks";
+
+export function useTodos() {
+  const queryClient = useQueryClient();
+
+  //get all Tasks`
+  const taskQuery = useQuery<Task[]>({
+    queryKey: ["tasks"],
+    queryFn:getAllTasks  
+  })
+
+  //add Task
+  const addMutationTodo = useMutation({
+    mutationFn: addTask,
+    onMutate: async (title: string) => {
+      await queryClient.cancelQueries({queryKey:['tasks']});
+
+      const prevTasks = queryClient.getQueryData<Task[]>(['tasks']) || [];
+      
+      const optimisticTodo: Task = {
+        id: Date.now(),
+        title,
+        completed:false,
+      }
+
+      queryClient.setQueryData<Task[]>(['tasks'], [
+        ...prevTasks,
+        optimisticTodo
+      ])
+
+      return {prevTasks}
+    },
+    onError:(_err, _title, context) => {
+      if(context?.prevTasks) {
+        queryClient.setQueryData(['todos'],context.prevTasks)
+      }
+    },
+    onSettled: () => queryClient.invalidateQueries({queryKey:['tasks']})
+  })
+
+  // toggle completed or uncompleted
+  const toggleMutationTodo = useMutation({
+    mutationFn: toggleTask,
+    onMutate: async (id:number) => {
+      await queryClient.cancelQueries({queryKey:['tasks']});
+
+      const prevTasks = queryClient.getQueryData<Task[]>(['tasks']) || [];
+
+      queryClient.setQueryData<Task[]>(
+        ['tasks'],
+        prevTasks.map((todo) => 
+          todo.id === id ? {...todo, completed: !todo.completed} : todo
+        )
+      );
+
+      return {prevTasks}
+    },
+    onError: (_err, _id, context) => {
+      if (context?.prevTasks) {
+        queryClient.setQueryData(['tasks'], context.prevTasks)
+      }
+    },
+    onSettled: () =>  queryClient.invalidateQueries({queryKey:['tasks']})
+  })
+
+  //delete task 
+
+  const removeMutationTodo = useMutation({
+    mutationFn: deleteTask,
+    onMutate: async (id: number) => {
+      await queryClient.cancelQueries({queryKey:['tasks']})
+      const prevTasks = queryClient.getQueryData<Task[]>(['tasks']) || [];
+
+      queryClient.setQueryData<Task[]>(
+        ['tasks'],
+        prevTasks.filter((todo) => todo.id !== id )
+      );
+      return  { prevTasks }
+    },
+    onError:(_err, _id, context ) => {
+      if (context?.prevTasks) {
+        queryClient.setQueryData(['tasks'], context.prevTasks)
+      }
+    }
+
+  })
+
+  return  {taskQuery, addMutationTodo, toggleMutationTodo, removeMutationTodo} 
+}
---
+++ todo-server/server.ts
@@ -0,0 +1,78 @@
+// server.ts
+const express = require("express");
+const cors = require("cors");
+
+interface Task {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+const app = express();
+const PORT = 4000;
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+
+
+app.get("/", (_req: any, res: any) => {
+  res.send("✅ Todo API работает! Используй /tasks");
+});
+
+// in-memory tasks
+let tasks: Task[] = [];
+let currentId = 1;
+
+// ✅ Get all tasks
+app.get("/tasks", (req: any, res: any) => {
+  res.json(tasks);
+});
+
+// ✅ Add task
+app.post("/tasks", (req: any, res: any) => {
+  const { title } = req.body;
+  if (!title) {
+    return res.status(400).json({ error: "Title is required" });
+  }
+
+  const newTask: Task = {
+    id: currentId++,
+    title,
+    completed: false,
+  };
+
+  tasks.push(newTask);
+  res.status(201).json(newTask);
+});
+
+// ✅ Toggle completed
+app.patch("/tasks/:id", (req: any, res: any) => {
+  const id = Number(req.params.id);
+  const task = tasks.find((t) => t.id === id);
+
+  if (!task) {
+    return res.status(404).json({ error: "Task not found" });
+  }
+
+  task.completed = !task.completed;
+  res.json(task);
+});
+
+// ✅ Delete task
+app.delete("/tasks/:id", (req: any, res: any) => {
+  const id = Number(req.params.id);
+  const exists = tasks.some((t) => t.id === id);
+
+  if (!exists) {
+    return res.status(404).json({ error: "Task not found" });
+  }
+
+  tasks = tasks.filter((t) => t.id !== id);
+  res.status(204).send();
+});
+
+// ✅ Start server
+app.listen(PORT, () => {
+  console.log(`🚀 Server running at http://localhost:${PORT}`);
+});
