91e4d5631e62399805fb81da60368830c4c35160
Sava1097 <sava10097@gmail.com>
2024-11-19 22:43:07 +0100

    remove useless file

--- todo-client/src/types/i18n.d.ts
+++
@@ -1,8 +0,0 @@
-// import "i18next";
-// import type { DefaultResources } from "../i18n";
-
-// declare module "i18next" {
-//   interface CustomTypeOptions {
-//     resources: DefaultResources["en"];
-//   }
-// }
