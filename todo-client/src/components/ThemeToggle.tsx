b2f16ff092bbf6f2ab71892f6cba4f57399cd25f
Sava1097 <sava10097@gmail.com>
2024-11-24 02:43:07 +0100

    feat(front): added localStorage for theme app, updated logics

--- todo-client/src/components/ThemeToggle.tsx
+++ todo-client/src/components/ThemeToggle.tsx
@@ -3,22 +3,30 @@ import { Button } from './ui/button';
 import { Sun, Moon } from 'lucide-react';
 
 export const ThemeToggle = () => {
-  const [isDark, setIsDark] = useState(false);
-  const root = document.documentElement;
+  const [isDark, setIsDark] = useState(() => {
+    if (typeof window === 'undefined') return false;
+    return localStorage.getItem('theme') === 'dark';
+  });
+
   useEffect(() => {
-    if (isDark) root.classList.add('dark');
-    else root.classList.remove('dark');
-  }, [isDark, root.classList]);
+    const root = document.documentElement;
+
+    if (isDark) {
+      root.classList.add('dark');
+      localStorage.setItem('theme', 'dark');
+    } else {
+      root.classList.remove('dark');
+      localStorage.setItem('theme', 'light');
+    }
+  }, [isDark]);
 
   return (
-    <div>
-      <Button
-        variant="outline"
-        onClick={() => setIsDark((prev) => !prev)}
-        className="flex items-center gap-2 hover:cursor-pointer"
-      >
-        {isDark ? <Sun size={12} /> : <Moon size={12} />}
-      </Button>
-    </div>
+    <Button
+      variant="outline"
+      onClick={() => setIsDark((prev) => !prev)}
+      className="flex items-center gap-2 hover:cursor-pointer"
+    >
+      {isDark ? <Sun size={12} /> : <Moon size={12} />}
+    </Button>
   );
 };
