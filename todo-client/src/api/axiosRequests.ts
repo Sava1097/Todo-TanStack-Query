64f64106979f77c167e0ff44aefc3214bf0c7d57
Sava1097 <sava10097@gmail.com>
2025-10-01 22:06:28 +0200

    fixed first part of remarks

--- todo-client/src/api/axiosRequests.ts
+++ todo-client/src/api/axiosRequests.ts
@@ -1,7 +1,5 @@
 import axios from "axios";
 
-// TODO: no need to call file as "axiosTasks" cause "axios" is about implementation
-// but file names should be more descriptive about the purpose
 export type Task = {
   id: number;
   title: string;
@@ -9,32 +7,28 @@ export type Task = {
 }
 
 const apiUrl = axios.create({
-  // TODO: get this from the .env file
-  baseURL: "http://localhost:4000", // backend
+  baseURL:  import.meta.env.VITE_API_URL, // backend
 });
 
 // get all tasks
 export const getAllTasks = async () => {
-  // TODO: I'm not sure if that work, but axios' methods should be generic
-  // and it's better to pass the type as a generic parameter
-  // apply same changes to other functions
   const { data } = await apiUrl.get<Task[]>("/tasks");
   return data;
 };    
 
 // add task
-export const addTask = async (title: string): Promise<Task> => {
-  const {data} = await apiUrl.post("/tasks", { title });
+export const addTask = async (title: string) => {
+  const {data} = await apiUrl.post<Task>("/tasks", { title });
   return data;
 };
 
 // edit task
-export const toggleTask = async (id: number): Promise<Task> => {
-  const { data } = await apiUrl.patch(`/tasks/${id}`);
+export const toggleTask = async (id: number) => {
+  const { data } = await apiUrl.patch<Task>(`/tasks/${id}`);
   return data;
 };
 
 // delete task 
-export const deleteTask = async (id: number): Promise<void> => {
-  await apiUrl.delete(`/tasks/${id}`);
+export const deleteTask = async (id: number) => {
+  await apiUrl.delete<Task>(`/tasks/${id}`);
 };
