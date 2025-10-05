54ac3729d7b2ed585c699cd7ef7e2f8aa013d184
Sava1097 <sava10097@gmail.com>
2025-10-05 21:11:27 +0200

    added trial language switcher

--- todo-client/src/routes/__root.tsx
+++ todo-client/src/routes/__root.tsx
@@ -1,13 +1,18 @@
 import { Outlet, Link } from "@tanstack/react-router";
 import { createRootRoute } from "@tanstack/react-router";
 import { Toaster  } from "@/components/ui/sonner";
+import { useTranslation } from "react-i18next";
+import { LanguageSwitcher } from "@/components/LanguageSwitcher";
+import "../i18n"
 
 function RootLayout() {
+  const { t } = useTranslation()
   return (
-    <div className="min-h-screen flex flex-col">
-      <nav className="flex justify-center gap-6 p-4 bg-gray-100 shadow">
-        <Link to="/" className="[&.active]:font-bold">Todos</Link>
-        <Link to="/about" className="[&.active]:font-bold">About</Link>
+    <div className="min-h-screen flex flex-col relative">
+      <nav className="flex justify-center items-center gap-6 p-4 md:text-2xl bg-gray-100 shadow">
+        <Link to="/" className="[&.active]:font-bold">{t("todos")}</Link>
+        <Link to="/about" className="[&.active]:font-bold">{t("about")}</Link>
+        <LanguageSwitcher/>
       </nav>
       <main className="flex-1 p-4">
         <Outlet />
---
+++ todo-client/src/types/i18n.d.ts
@@ -0,0 +1,8 @@
+import "i18next";
+import type { DefaultResources } from "../i18n";
+
+declare module "i18next" {
+  interface CustomTypeOptions {
+    resources: DefaultResources["en"];
+  }
+}
