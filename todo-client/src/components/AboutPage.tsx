5ec3bac14f1c90e7ba33158358edc1e8092df826
Sava1097 <sava10097@gmail.com>
2025-10-06 11:45:24 +0200

    added tasks counter, framer-motion

--- todo-client/src/components/AboutPage.tsx
+++ todo-client/src/components/AboutPage.tsx
@@ -3,7 +3,7 @@ export default function AboutPage() {
   return (
     <div className="min-h-screen lg:h-auto flex flex-col items-center justify-center gap-2 p-6 bg-white shadow rounded-lg">
       <h2 className="text-2xl text-center font-bold">About This App</h2>
-      <p className="text-gray-700">
+      <p className="text-gray-700 lg:text-2xl text-balance">
         This is a simple Todo List project built with React, TanStack Query,
         TanStack Router, Tailwind, and ShadCN UI.
       </p>
